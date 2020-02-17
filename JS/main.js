7adde920ca384e39006cf61f01f297b1fdb4d71f
OlesiaVysotskaia <olesiavysorskaia@gmail.com>
2020-02-17 16:14:21 +0300

    сделала морфинг

---
+++ JS/main.js
@@ -0,0 +1,11 @@
+var morphing = anime({
+  targets: 'polygon',
+  points: [
+    { value: '715 0 0 326 166 478 802 40 715 0' },
+    { value: '142.14 1041.41 0 992.39 402.75 0 513.02 44.75 142.14 1041.41' },
+  ],
+  easing: 'easeOutQuint'	,
+  duration: 4000,
+  direction: 'alternate',
+  loop: true
+});
