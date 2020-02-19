8b36531e51e341311ffa1bf8b4946ad7081ea772
OlesiaVysotskaia <olesiavysorskaia@gmail.com>
2020-02-19 20:10:50 +0300

    Добавила первые анимации

--- JS/main.js
+++ JS/main.js
@@ -1,11 +1,136 @@
 var morphing = anime({
-  targets: 'polygon',
+  targets: '.blackRectBigPath',
   points: [
-    { value: '715 0 0 326 166 478 802 40 715 0' },
-    { value: '142.14 1041.41 0 992.39 402.75 0 513.02 44.75 142.14 1041.41' },
+    { value: '722 7 0 0 218 214 836 74 722 7' },
+    { value: '0 243 244 0 695 452 452 695 0 243' },
   ],
-  easing: 'easeOutQuint'	,
+  easing: 'easeInOutQuad'	,
   duration: 4000,
   direction: 'alternate',
   loop: true
 });
+
+var position = anime({
+  targets: '.blackRectBig',
+  translateY: [171,65],
+  translateX: [260,405],
+  easing: 'easeInOutQuad'	,
+  duration: 4000,
+  direction: 'alternate',
+  loop: true
+})
+
+var morphing = anime({
+  targets: '.redCircle',
+  width:[422,81],
+  height:[422,81],
+  easing: 'easeInOutQuad'	,
+  duration: 4000,
+  direction: 'alternate',
+  loop: true
+});
+
+var position = anime({
+  targets: '.blackRectBig',
+  translateY: [-640,407],
+  translateX: [910,347],
+  easing: 'easeInOutQuad'	,
+  duration: 4000,
+  direction: 'alternate',
+  loop: true
+})
+//
+// var morphing = anime({
+//   targets: '.yellowRectPath',
+//   points: [
+//     { value: '681 0 733 0 57 424 0 363 681 0' },
+//     { value: '0 1037 148 1113 682 76 534 0 0 1037' },
+//   ],
+//   easing: 'easeInOutQuad'	,
+//   duration: 4000,
+//   direction: 'alternate',
+//   loop: true
+// });
+
+// let position = anime({
+//   targets: '.blackRectBig',
+//   height: ['205px','700px'],
+//   easing: 'easeInOutQuad'	,
+//   duration: 4000,
+//   direction: 'alternate',
+//   loop: true,
+// })
+
+// var morphing = anime({
+//   targets: '.blackLineHorPath',
+//   points: [
+//     { value: '848 0 852 8 4 425 0 417 848 0' },
+//     { value: '0 31 32 0 342 309 310 341 0 31' },
+//   ],
+//   easing: 'easeInOutQuad'	,
+//   duration: 4000,
+//   direction: 'alternate',
+//   loop: true
+// });
+//
+// var morphing = anime({
+//   targets: '.blackRectMedPath',
+//   points: [
+//     { value: '0 103 232 0 246 27 9 118 0 103' },
+//     { value: '16 0 0 7 295 260 301 252 6 0' },
+//   ],
+//   easing: 'easeInOutQuad'	,
+//   duration: 4000,
+//   direction: 'alternate',
+//   loop: true
+// });
+
+
+
+// var morphing = anime({
+//   targets: '.blackRectBigPath',
+//   points: [
+//     { value: '722 7 0 0 218 214 836 74 722 7' },
+//     { value: '142.14 1041.41 0 992.39 402.75 0 513.02 44.75 142.14 1041.41' },
+//   ],
+//   easing: 'easeInSine'	,
+//   duration: 4000,
+//   direction: 'alternate',
+//   loop: true
+// });
+//
+// var morphing = anime({
+//   targets: '.blackRectBigPath',
+//   points: [
+//     { value: '722 7 0 0 218 214 836 74 722 7' },
+//     { value: '142.14 1041.41 0 992.39 402.75 0 513.02 44.75 142.14 1041.41' },
+//   ],
+//   easing: 'easeInSine'	,
+//   duration: 4000,
+//   direction: 'alternate',
+//   loop: true
+// });
+//
+// var morphing = anime({
+//   targets: '.blackRectBigPath',
+//   points: [
+//     { value: '722 7 0 0 218 214 836 74 722 7' },
+//     { value: '142.14 1041.41 0 992.39 402.75 0 513.02 44.75 142.14 1041.41' },
+//   ],
+//   easing: 'easeInSine'	,
+//   duration: 4000,
+//   direction: 'alternate',
+//   loop: true
+// });
+//
+// var morphing = anime({
+//   targets: '.blackRectBigPath',
+//   points: [
+//     { value: '722 7 0 0 218 214 836 74 722 7' },
+//     { value: '142.14 1041.41 0 992.39 402.75 0 513.02 44.75 142.14 1041.41' },
+//   ],
+//   easing: 'easeInSine'	,
+//   duration: 4000,
+//   direction: 'alternate',
+//   loop: true
+// });
